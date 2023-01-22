6078819b53ec7c40ba13757fcb4ead8090959853
Ran Fang <rf421@imperial.ac.uk>
2023-01-23 05:33:10 +0800

    is prime tempt2

--- math_utils/primes.py
+++ math_utils/primes.py
@@ -1,6 +1,9 @@
 from math import sqrt
 def isprime(x):
-    for i in range(2,int(sqrt(x)+1)):
-        if x%i==0:
-            return False
-    return True
+    if x==1:
+        return False
+    else:
+        for i in range(2,int(sqrt(x)+1)):
+            if x%i==0:
+                return False
+        return True
